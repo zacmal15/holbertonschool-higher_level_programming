07191853b666724231a59e48406f0280f7d7b8d7
Zac <13089@holbertonstudents.com>
2026-05-05 08:15:23 +0000

    added posorneg file

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+# Check if number is positive, zero, or negative and print
+if number > 0:
+    print(f"{number} is positive")
+elif number == 0:
+    printf(f"{number} is zero")
+else:
+    print(f"{number} is negative")
+    

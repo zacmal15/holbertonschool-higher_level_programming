9a15dcad979b81d41358aaa509e080250479fb83
Zac <13089@holbertonstudents.com>
2026-05-05 06:07:12 +0000

    fixed message to be displayed

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -7,7 +7,7 @@ def main():
     """
     Main function that prints required message
     """
-print("Programming is like building a multilingual puzzle ,")
+print("Programming is like building a multilingual puzzle")
 
 if __name__ == "__main__":
     main()

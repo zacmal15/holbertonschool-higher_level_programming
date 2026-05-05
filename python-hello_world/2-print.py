a4b583bef5ac852e4a63281bc66ade1b7b71006a
Zac <13089@holbertonstudents.com>
2026-05-05 06:21:36 +0000

    simplified code

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,13 +1,2 @@
 #!/usr/bin/python3
-"""
-This script prints a specific string using the print function
-"""
-
-def main():
-    """
-    Main function that prints required message
-    """
-print("Programming is like building a multilingual puzzle")
-
-if __name__ == "__main__":
-    main()
+print("\"Programming is like building a multilingual puzzle")

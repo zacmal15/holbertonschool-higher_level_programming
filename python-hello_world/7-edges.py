853f2e2f3d41ba6a3be512c9c26fa38a420f1756
Zac <13089@holbertonstudents.com>
2026-05-05 07:39:07 +0000

    fix

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
-# Assign string to variable 'word'
 word = "Holberton"
-
-# Get first 3 characters of string
 word_first_3 = word[:3]
-
-# Get the last 2 characters of string
 word_last_2 = word[-2:]
-
-# Get string without first and last characters
 middle_word = word[1:-1]
-
-#Print results
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")

9ddfb96a67a2cc4ce57a51506357d150960dc6ba
Zac <13089@holbertonstudents.com>
2026-05-05 07:44:02 +0000

    added eighth file

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+# Extract and rearrange parts of str
+print(str[39:66] + str[:6])

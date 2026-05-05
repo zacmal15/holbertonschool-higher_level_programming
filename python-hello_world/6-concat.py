5ac79736efbb8feb07a6f9b2a779d359dffdf40e
Zac <13089@holbertonstudents.com>
2026-05-05 08:07:14 +0000

    fixxxxx

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-#str1 and str2 are used toegether to form a string
-print(f"Welcome to {str1} {str2}!")
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")

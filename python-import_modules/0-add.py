dc24e1475972b1326f3cd7acf6eabc30af0fce28
Zac <13089@holbertonstudents.com>
2026-05-07 08:47:53 +0000

    fixed indentation

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 from add_0 import add
 
 if __name__ == "__main__":
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))

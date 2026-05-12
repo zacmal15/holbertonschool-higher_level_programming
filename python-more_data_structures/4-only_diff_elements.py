b9378681e8fc20497a41c884f58bcea397a07a73
Zac <13089@holbertonstudents.com>
2026-05-12 07:15:43 +0000

    fixed comment spacing

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    return set_1 ^ set_2 # ^ means return values that are not shared
+    return set_1 ^ set_2  # ^ means return values that are not shared

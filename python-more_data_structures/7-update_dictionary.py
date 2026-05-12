a05dba95e8ea45a06cf3148d6238ea08a8f9a0c9
Zac <13089@holbertonstudents.com>
2026-05-12 07:31:24 +0000

    added inline comment

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    a_dictionary[key] = value  # this line checks if a key exists AND a value otherwise it will create/replace
 
     return a_dictionary

d336ae0dada37a0b1e27d55fc88fadef779c60cf
Zac <13089@holbertonstudents.com>
2026-05-12 07:50:57 +0000

    added inline comment

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,7 +7,7 @@ def best_score(a_dictionary):
     best_value = 0
 
     for key in a_dictionary:
-        if a_dictionary[key] > best_value:
+        if a_dictionary[key] > best_value:  # a_dictionary[key] accesses the value stored in the key
             best_value = a_dictionary[key]
             best_key = key
 

2deb54f39c9866223b04c41faf402f059223d177
Zac <13089@holbertonstudents.com>
2026-05-13 07:28:53 +0000

    fixed errors

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,11 +4,11 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for i in range(x):
-            print{"{}".format(my_list[i]), end="")
+            print("{}".format(my_list[i]), end="")
             count += 1
-        except IndexError:
-            pass
+    except IndexError:
+        pass
 
-        print()
+    print()
 
-        return count
+    return count

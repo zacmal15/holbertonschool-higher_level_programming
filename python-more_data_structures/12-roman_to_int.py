00446dda66ee1d7d5014e1c9ecd0a45860ccfb42
Zac <13089@holbertonstudents.com>
2026-05-14 06:36:56 +0000

    fixed roman file

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,30 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    
+    if type(roman_string) != str or roman_string is None:
+        return 0
+
+    roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    previous = 0
+
+    for char in reversed(roman_string):
+        value = roman(char)
+
+        if value < previous:
+            total -= value
+        else:
+            total += value
+
+        previous = value
+
+    return total
+ 

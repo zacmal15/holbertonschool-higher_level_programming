8829302ca2af351e8370fe2c9b1be810123ed0c2
Zac <13089@holbertonstudents.com>
2026-05-14 06:43:31 +0000

    fixed line 3

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or roman_string is None:
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
 
     roman = {

ab7c698745fb42d6ac4eeebbf3acef1fdaefb301
Zac <13089@holbertonstudents.com>
2026-05-15 07:24:00 +0000

    added print full name file

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module contains a function that prints a full name."""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints My name is followed by first and last name."""
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))

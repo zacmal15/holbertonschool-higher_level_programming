6d965eb7844c37e1e33932bb8d2a8036ad8eb47c
Zac <13089@holbertonstudents.com>
2026-05-15 07:42:11 +0000

    added text indentation file

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module contains a function that prints formatted text."""
+
+
+def text_indentation(text):
+    """Prints text with 2 new lines after .,? and :"""
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    new_text = ""
+
+    for char in text:
+
+        new_text += char
+
+        if char in ".?:":
+            new_text += "\n\n"
+
+    lines = new_text.split("\n")
+
+    for line in lines:
+        print(line.strip())

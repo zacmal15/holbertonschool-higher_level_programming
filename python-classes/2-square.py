2a03d56432776d2e9098f24edcd99a884f124d72
Zac <13089@holbertonstudents.com>
2026-05-19 05:18:45 +0000

    added third square file

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size=0):
+        """Initialise a square with a size."""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size

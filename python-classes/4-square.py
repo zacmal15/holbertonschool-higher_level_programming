a55ffb28bfe0548cf560fb678840c5df9b155f1f
Zac <13089@holbertonstudents.com>
2026-05-19 06:29:54 +0000

    added next square file

---
+++ python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size=0):
+        """Initialises a square with a size."""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrive the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+
+        return self.__size * self.__size

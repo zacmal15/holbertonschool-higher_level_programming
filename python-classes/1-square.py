8986031cfd7045d5fc417567cb67be07a4a3bf8b
Zac <13089@holbertonstudents.com>
2026-05-19 04:55:26 +0000

    added second classes file

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a square class."""
+
+
+class Square:
+    """This class defines a square."""
+
+    def __init__(self, size):
+        """Initialises a square with a size."""
+        self.__size = size

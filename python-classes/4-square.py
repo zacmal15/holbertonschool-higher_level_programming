bdc152622fb0b0db9d5dc17c238008ed7c9da57c
Zac <13089@holbertonstudents.com>
2026-05-19 06:37:33 +0000

    fixed white space

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,12 +6,12 @@ class Square:
     """This class defines a square."""
 
     def __init__(self, size=0):
-        """Initialises a square with a size."""
+        """Initializes a square with a size."""
         self.size = size
 
     @property
     def size(self):
-        """Retrive the size of the square."""
+        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
@@ -23,10 +23,9 @@ class Square:
 
         if value < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = value
 
     def area(self):
         """Return the current square area."""
-
         return self.__size * self.__size

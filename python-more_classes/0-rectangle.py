99d5b7eef221547e7e4d071b4395a5a557f123e4
Zac <13089@holbertonstudents.com>
2026-05-20 05:54:39 +0000

    fixed docstring

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""This module defines an empty class named Rectangle"""
+
+
 class Rectangle:
-    """This class defines a rectangle"""
+    """This class defines an empty rectangle."""
     pass

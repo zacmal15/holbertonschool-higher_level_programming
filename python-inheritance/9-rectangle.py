6c6a1e6b471d3158ee7ef32377e5477dc89490eb
Zac <13089@holbertonstudents.com>
2026-05-26 07:14:45 +0000

    syntax typo

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -4,7 +4,7 @@
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
-class rectangle(BaseGeometry):
+class Rectangle(BaseGeometry):
     """This class represents a rectangle."""
 
     def __init__(self, width, height):

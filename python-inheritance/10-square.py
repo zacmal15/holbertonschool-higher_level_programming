3b4277c3d29770388bc967ec06f6e2806078e5c7
Zac <13089@holbertonstudents.com>
2026-05-26 07:19:12 +0000

    added square file

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """This class represents a square."""
+
+    def __init__(self, size):
+        """Initialises square with validated size."""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size * self.__size

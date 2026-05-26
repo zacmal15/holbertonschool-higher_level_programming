e6581b010fd95c3302f7022aaf73f9343cedb10f
Zac <13089@holbertonstudents.com>
2026-05-26 05:22:54 +0000

    added basegeometry file with exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a BaseGeometry class."""
+
+
+class BaseGeometry:
+    """This class represents the base geometry."""
+
+    def area(self):
+        """Raises an exception because area is not implemented."""
+        raise Exception("area() is not implemented")

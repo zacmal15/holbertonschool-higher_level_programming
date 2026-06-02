b0e4c39a15b85e5b0361f124d67e9c2de8ead8d6
Zac <13089@holbertonstudents.com>
2026-06-02 07:54:33 +0000

    added student class module

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines a Student class."""
+
+
+class Student:
+    """This class represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialises a student with a first, last name and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return the dictionary representation of the Student instance."""
+        return self.__dict__

52e18d38b37d32a2fa8056f360e686b29a25dfa7
Zac <13089@holbertonstudents.com>
2026-06-02 05:21:16 +0000

    added script that adds all arguments

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This script adds command line arguments to a JSON file."""
+
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+
+save_to_json_file(my_list, filename)

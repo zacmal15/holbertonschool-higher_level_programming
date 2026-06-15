75067b05383e2c9b27e1ff312c13bf0d377c646e
zac <13089@holbertonstudents.com>
2026-06-15 10:51:59 +0000

    fixed syntax

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.genre_id
+ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = 'Dexter'

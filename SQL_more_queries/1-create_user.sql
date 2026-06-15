1b7020eb12b4ea46bf17e96bab9af3f5aa654241
zac <13089@holbertonstudents.com>
2026-06-15 08:37:36 +0000

    fixed syntax

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *,* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';

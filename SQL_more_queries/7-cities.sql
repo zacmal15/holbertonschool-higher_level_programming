f9e6d3288c22122c51f2d6faeae0e03af57097db
zac <13089@holbertonstudents.com>
2026-06-15 10:14:14 +0000

    added cities file

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Create the database hbtn_0d_usa if doesnt exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Create the table cities if it doesnt exist
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);

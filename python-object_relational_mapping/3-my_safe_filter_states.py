242855fac4c1eb3c73a55eb35467f238abb20d3c
Zac <13089@holbertonstudents.com>
2026-06-22 06:44:45 +0000

    added safe filter file

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Lists all states matching a provided name safely."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states "
+        "WHERE name = %s "
+        "ORDER BY id ASC",
+        (sys.argv[4],)
+    )
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()

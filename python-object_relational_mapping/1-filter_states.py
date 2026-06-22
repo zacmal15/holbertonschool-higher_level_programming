a9cb25f8ea70bf77abeb2e3717050f50424ebfb6
Zac <13089@holbertonstudents.com>
2026-06-22 06:20:20 +0000

    added filtered states file

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Lists all states starting with N from the database."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states "
+        "WHERE name LIKE 'N%' "
+        "ORDER BY id ASC"
+    )
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()

5d05cd9a347d8f2b73700a0d1d592b03767d9bf2
Zac <13089@holbertonstudents.com>
2026-06-22 06:51:20 +0000

    added city list from database

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Lists all cities from the database."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM cities "
+        "ORDER BY id ASC"
+    )
+
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()

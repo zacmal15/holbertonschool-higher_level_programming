f2e13eadee458ea12415eee3d37133d3a62133e8
Zac <13089@holbertonstudents.com>
2026-06-23 05:25:41 +0000

    added script to insert louisiana to db

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Adds the state object louisiana to database."""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        )
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+
+    session.add(new_state)
+
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()

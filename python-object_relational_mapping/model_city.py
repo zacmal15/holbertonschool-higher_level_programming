f47167a245f4f26af9d1637235bb239c75bd019e
Zac <13089@holbertonstudents.com>
2026-06-23 07:47:07 +0000

    added city model script

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This module defines the cirty class."""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """This class represents te cities table."""
+
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_ley=True,
+                autoincrement=True, nullable=False)
+
+    name = Column(String(128), nullable=False)
+
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)

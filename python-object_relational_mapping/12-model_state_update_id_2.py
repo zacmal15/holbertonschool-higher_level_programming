75dcf9049ef8209d4b7a13e39609adb36d4a8b79
Zac <13089@holbertonstudents.com>
2026-06-23 07:32:45 +0000

    fixed syntax

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -7,7 +7,7 @@ from model_state import Base, State
 import sys
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
             sys.argv[1],

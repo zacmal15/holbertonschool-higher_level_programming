15f5344e43bf4503cd433c98adbe9376b419396a
Zac <13089@holbertonstudents.com>
2026-06-23 08:01:58 +0000

    fixed syntax

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -10,7 +10,7 @@ class City(Base):
 
     __tablename__ = "cities"
 
-    id = Column(Integer, primary_ley=True,
+    id = Column(Integer, primary_key=True,
                 autoincrement=True, nullable=False)
 
     name = Column(String(128), nullable=False)

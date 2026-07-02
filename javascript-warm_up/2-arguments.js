9e2a3a751a7ebb972bee05fe583f7fc948f164a9
ZAc <13089@holbertonstudents.com>
2026-07-02 07:24:35 +0000

    i hate checker

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@
 const argCount = process.argv.length -2;
 
 if (argCount === 0) {
-    console.log('No Argument');
+    console.log('No argument');
 } else if (argCount === 1) {
     console.log('Argument found');
 } else {

e4187269e27b7544b1bfc48285ace7ac893a7468
ZAc <13089@holbertonstudents.com>
2026-07-02 07:26:24 +0000

    semistandard issue

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-const argCount = process.argv.length -2;
+const argCount = process.argv.length - 2;
 
 if (argCount === 0) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (argCount === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }

b636a6b4eb715218eb000d7014d8979c46e9e770
Zac <13089@holbertonstudents.com>
2026-07-03 06:46:54 +0000

    indent issue

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -6,6 +6,6 @@ if (isNaN(occurrences)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < occurrences; i++) {
-  console.log('C is fun');
+    console.log('C is fun');
   }
 }

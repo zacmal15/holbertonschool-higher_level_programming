6c9bd8f9ab3d434b02cbc5136709f032c7863e4e
Zac <13089@holbertonstudents.com>
2026-07-03 06:53:34 +0000

    fixed spacing

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -6,10 +6,10 @@ if (isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
-    let row = ' ';
+    let row = '';
 
     for (let j = 0; j < size; j++) {
-        row += 'X';
+      row += 'X';
     }
 
     console.log(row);

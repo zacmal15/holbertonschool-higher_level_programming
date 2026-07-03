8e04226fed9a344ce604509b4992953033287392
Zac <13089@holbertonstudents.com>
2026-07-03 07:11:43 +0000

    added second largets number file

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+let largest = -Infinity;
+let secondLargest = -Infinity;
+
+for (let i = 2; i < ProcessingInstruction.argv.length; i++) {
+  const current = parseInt(process.argv[i]);
+
+  if (current > largest) {
+    secondLargest = largest;
+    largest = current;
+  } else if (current > secondLargest && current != largest) {
+    secondLargest = current;
+  }
+}
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+    console.log(secondLargest);
+}
